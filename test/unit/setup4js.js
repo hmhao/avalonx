9c13036f8fbdbbab92fb073178d461f7200ff227
hmh <215795583@qq.com>
2017-05-10 14:18:30 +0800

    单元测试setup适应avalon 增加setup4js.js，可使用node调试

--- test/unit/setup4js.js
+++ test/unit/setup4js.js
@@ -1,16 +1,14 @@
-import 'babel-polyfill'
-import avalon from 'avalon2/dist/avalon.js'
-import Avalonx from '../../dist/avalonx.js'
-
 global.name = ''//avalon的bug,这里要设置一下
 
+require('babel-polyfill')
+var avalon = require('avalon2/dist/avalon.js')
 avalon.config({debug: true})
 var avalonDefine = avalon.define
 avalon.define = function (definition) {
   if(definition.computed){
     definition['$computed'] = definition['$computed'] || {}
     avalon.mix(definition['$computed'], definition.computed)
-      delete definition.computed
+    delete definition.computed
   }
   if(definition.methods){
     avalon.mix(definition, definition.methods)
@@ -18,3 +16,9 @@ avalon.define = function (definition) {
   }
   return avalonDefine.call(avalon, definition)
 }
+
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+jasmine.loadConfigFile(__dirname + '/jasmine.json');
+jasmine.execute();
